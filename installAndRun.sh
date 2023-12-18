4ff324ecbce740f0cdb483b89e97b7399f174558
Ian Kersz <kerszamaral@gmail.com>
2023-12-18 00:09:48 -0300

    Added server Ip

--- installAndRun.sh
+++ installAndRun.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # this script is used to install and run the server
+ip_address=$(hostname -I | awk '{print $1}')
 python3 -m venv venv &&
 source venv/bin/activate &&
 pip install -r requirements.txt &&
@@ -7,4 +8,5 @@ cd src &&
 python manage.py migrate &&
 python manage.py create_groups &&
 python manage.py createsuperuser &&
+echo "The server is running at http://$ip_address:8000" &&
 python manage.py runserver 0.0.0.0:8000 --insecure
