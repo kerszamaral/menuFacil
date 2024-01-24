4e5b560bae0855aa16a3faa5b7d84ac9793378e4
Ian Kersz <kerszamaral@gmail.com>
2024-01-24 20:17:40 -0300

    Users now automatically get tab and cart

--- src/account/models.py
+++ src/account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.conf import settings
+
+from cart.models import Cart
+from tab.models import Tab
 
 # Create your models here.
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_fields(sender, instance, created, **kwargs):
+    if created:
+        Cart.objects.create(client=instance)
+        Tab.objects.create(client=instance)
+        instance.cart.save()
+        instance.tab.save()
--- src/menuFacil/validation.py
+++ src/menuFacil/validation.py
@@ -1,5 +1,6 @@
 
 from django.http import HttpRequest
+from django.apps import apps
 
 CART_KEY = 'cart_token'
 TAB_KEY = 'tab_token'
@@ -7,15 +8,18 @@ CART_REDIRECT_URL = "cart:create"
 TAB_REDIRECT_URL = "BBBB"
 
 def cart_token_exists(request: HttpRequest) -> bool:
-    if CART_KEY in request.session.keys():
-        return True
-
     if request.user.is_authenticated:
         cart = request.user.cart # type: ignore
         request.session[CART_KEY] = cart.id
         return True
-
-    return False
+    
+    if CART_KEY in request.session.keys():
+        return True
+    
+    model = apps.get_model(app_label='cart', model_name='Cart')
+    cart = model.objects.create()
+    request.session[CART_KEY] = cart.id # type: ignore
+    return True
 
 def tab_token_exists(request: HttpRequest) -> bool:
     if TAB_KEY in request.session.keys():
